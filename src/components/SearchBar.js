3810659b5e39ae8ab09b0c09a93c78b64130559d
Mohsen Hussein <mohsenhussen8@gmail.com>
2021-11-15 17:31:54 +0200

    add project files

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                Search Bar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
